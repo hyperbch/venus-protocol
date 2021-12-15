554cae15739ecca8d67230a422ba76a4da91638c
lakshmi <defipool@gmail.com>
2021-12-15 15:11:40 +0800

    update comptroller-harness with new function from Liquidator update assertions in liquidatorTest update Liquidator contract with validations

--- tests/Tokens/liquidatorTest.js
+++ tests/Tokens/liquidatorTest.js
@@ -4,21 +4,32 @@ const {
   bnbMantissa
 } = require('../Utils/BSC');
 
+
+const { dfn } = require('../Utils/JS');
 const {
   makeVToken,
   fastForward,
   setBalance,
   getBalances,
   adjustBalances,
-  pretendBorrow,
-  preApprove
+  pretendBorrow
 } = require('../Utils/Venus');
 
 const repayAmount = bnbUnsigned(10e2);
 const seizeAmount = repayAmount;
 const seizeTokens = seizeAmount.mul(4); // forced
+const announcedIncentive = bnbMantissa('1.10');
+const treasuryPercent = bnbMantissa('0.05');
 
-async function preLiquidate(vToken, liquidator, borrower, repayAmount, vTokenCollateral) {
+async function preApprove(vToken, from, spender, amount, opts = {}) {
+  if (dfn(opts.faucet, true)) {
+    expect(await send(vToken.underlying, 'harnessSetBalance', [from, amount], { from })).toSucceed();
+  }
+
+  return send(vToken.underlying, 'approve', [spender, amount], { from });
+}
+
+async function preLiquidate(liquidatorContract, vToken, liquidator, borrower, repayAmount, vTokenCollateral) {
   // setup for success in liquidating
   await send(vToken.comptroller, 'setLiquidateBorrowAllowed', [true]);
   await send(vToken.comptroller, 'setLiquidateBorrowVerify', [true]);
@@ -27,6 +38,7 @@ async function preLiquidate(vToken, liquidator, borrower, repayAmount, vTokenCol
   await send(vToken.comptroller, 'setSeizeAllowed', [true]);
   await send(vToken.comptroller, 'setSeizeVerify', [true]);
   await send(vToken.comptroller, 'setFailCalculateSeizeTokens', [false]);
+  await send(vToken.comptroller, 'setAnnouncedLiquidationIncentiveMantissa', [announcedIncentive]);
   await send(vToken.underlying, 'harnessSetFailTransferFromAddress', [liquidator, false]);
   await send(vToken.interestRateModel, 'setFailBorrowRate', [false]);
   await send(vTokenCollateral.interestRateModel, 'setFailBorrowRate', [false]);
@@ -35,84 +47,78 @@ async function preLiquidate(vToken, liquidator, borrower, repayAmount, vTokenCol
   await setBalance(vTokenCollateral, borrower, seizeTokens);
   await pretendBorrow(vTokenCollateral, borrower, 0, 1, 0);
   await pretendBorrow(vToken, borrower, 1, 1, repayAmount);
-  await preApprove(vToken, liquidator, repayAmount);
+  await preApprove(vToken, liquidator, liquidatorContract._address, repayAmount);
 }
 
-async function liquidateFresh(vToken, liquidator, borrower, repayAmount, vTokenCollateral) {
-  return send(vToken, 'harnessLiquidateBorrowFresh', [liquidator, borrower, repayAmount, vTokenCollateral._address]);
-}
-
-async function liquidate(vToken, liquidator, borrower, repayAmount, vTokenCollateral) {
+async function liquidate(liquidatorContract, vToken, liquidator, borrower, repayAmount, vTokenCollateral) {
   // make sure to have a block delta so we accrue interest
   await fastForward(vToken, 1);
   await fastForward(vTokenCollateral, 1);
-  return send(vToken, 'liquidateBorrow', [borrower, repayAmount, vTokenCollateral._address], { from: liquidator });
+  return send(
+    liquidatorContract,
+    'liquidateBorrow',
+    [vToken._address, borrower, repayAmount, vTokenCollateral._address],
+    { from: liquidator }
+  );
 }
 
-async function seize(vToken, liquidator, borrower, seizeAmount) {
-  return send(vToken, 'seize', [liquidator, borrower, seizeAmount]);
+function calculateSplitSeizedTokens(amount) {
+  const treasuryDelta =
+      amount
+        .mul(bnbMantissa('1')).div(announcedIncentive) // / 1.1
+        .mul(treasuryPercent).div(bnbMantissa('1'));   // * 0.05
+  const liquidatorDelta = amount.sub(treasuryDelta);
+  return { treasuryDelta, liquidatorDelta };
 }
 
-describe('VToken', function () {
+describe('Liquidator', function () {
   let root, liquidator, borrower, treasury, accounts;
-  let vToken, vBNB, vTokenCollateral;
-  let liquidatorContract;
+  let vToken, vTokenCollateral, liquidatorContract;
 
   beforeEach(async () => {
     [root, liquidator, borrower, treasury, ...accounts] = saddle.accounts;
     vToken = await makeVToken({ comptrollerOpts: { kind: 'bool' } });
-    vBNB = await makeVToken({ kind: "vbnb", supportMarket: true });
     vTokenCollateral = await makeVToken({ comptroller: vToken.comptroller });
-    liquidatorContract = await deploy('Liquidator',
-      [
-        vBNB._address,
+    vBnb = await makeVToken({ kind: 'vbnb', comptroller: vToken.comptroller });
+    liquidatorContract = await deploy(
+      'Liquidator', [
+        root,
+        vBnb._address,
         vToken.comptroller._address,
         treasury,
-        bnbMantissa(0.05)
-      ]);
-    console.log(`liquidator is deployed at: ${liquidatorContract._address}`);
-  });
-
-  beforeEach(async () => {
-    await preLiquidate(vToken, liquidator, borrower, repayAmount, vTokenCollateral);
+        treasuryPercent
+      ]
+    );
+    await preLiquidate(liquidatorContract, vToken, liquidator, borrower, repayAmount, vTokenCollateral);
   });
 
   describe('liquidateBorrow', () => {
+    it('returns success from liquidateBorrow and transfers the correct amounts', async () => {
+      const beforeBalances = await getBalances([vToken, vTokenCollateral], [treasury, liquidator, borrower]);
+      const result = await liquidate(liquidatorContract, vToken, liquidator, borrower, repayAmount, vTokenCollateral);
+      const gasCost = await bnbGasCost(result);
+      const afterBalances = await getBalances([vToken, vTokenCollateral], [treasury, liquidator, borrower]);
 
-    it("returns success from liquidateBorrow and transfers the correct amounts", async () => {
-
-      const beforeBalances = await getBalances([vToken, vTokenCollateral], [liquidator, borrower]);
-
-      //const result = await liquidate(vToken, liquidator, borrower, repayAmount, vTokenCollateral);
-
-      const result = await send(liquidatorContract, 'liquidateBorrow', [vToken._address, borrower, bnbUnsigned(0), vTokenCollateral._address]);
-
-      expect(result).toSucceed();
-
-      console.log(`result is: ${JSON.stringify(result)}`);
-
+      const { treasuryDelta, liquidatorDelta } = calculateSplitSeizedTokens(seizeTokens);
       expect(result).toHaveLog('LiquidateBorrowedTokens', {
-        liquidator: liquidator, 
-        borrower: borrower,
-        repayAmount: bnbUnsigned(0),
+        liquidator,
+        borrower,
+        repayAmount: repayAmount.toString(),
         vTokenCollateral: vTokenCollateral._address,
-        seizeTokensForTreasury: bnbUnsigned(0),
-        seizeTokensForLiquidator: bnbUnsigned(0)
+        seizeTokensForTreasury: treasuryDelta.toString(),
+        seizeTokensForLiquidator: liquidatorDelta.toString()
       });
-
-      // const gasCost = await bnbGasCost(result);
-      // const afterBalances = await getBalances([vToken, vTokenCollateral], [liquidator, borrower]);
-      // expect(afterBalances).toEqual(await adjustBalances(beforeBalances, [
-      //   [vToken, 'cash', repayAmount],
-      //   [vToken, 'borrows', -repayAmount],
-      //   [vToken, liquidator, 'bnb', -gasCost],
-      //   [vToken, liquidator, 'cash', -repayAmount],
-      //   [vTokenCollateral, liquidator, 'bnb', -gasCost],
-      //   [vTokenCollateral, liquidator, 'tokens', seizeTokens],
-      //   [vToken, borrower, 'borrows', -repayAmount],
-      //   [vTokenCollateral, borrower, 'tokens', -seizeTokens]
-      // ]));
+      expect(afterBalances).toEqual(await adjustBalances(beforeBalances, [
+        [vToken, 'cash', repayAmount],
+        [vToken, 'borrows', -repayAmount],
+        [vToken, liquidator, 'bnb', -gasCost],
+        [vToken, liquidator, 'cash', -repayAmount],
+        [vTokenCollateral, liquidator, 'bnb', -gasCost],
+        [vTokenCollateral, liquidator, 'tokens', liquidatorDelta],
+        [vTokenCollateral, treasury, 'tokens', treasuryDelta],
+        [vToken, borrower, 'borrows', -repayAmount],
+        [vTokenCollateral, borrower, 'tokens', -seizeTokens]
+      ]));
     });
   });
-
 });
